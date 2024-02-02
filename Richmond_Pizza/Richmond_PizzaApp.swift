9404675eb085986c21fc4ec4a3f50f944551cc25
SeanTheJedi <richmondgyimah5@gmail.com>
2024-02-02 13:58:48 -0500

    Initial Commit

---
+++ Richmond_Pizza/Richmond_PizzaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Richmond_PizzaApp.swift
+//  Richmond_Pizza
+//
+//  Created by Richmond Gyimah on 02/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct Richmond_PizzaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
